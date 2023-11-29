bdf7a23b24a32905aff2d6d529a1ce68200ab607
industrial-demon <150183500+industrial-demon@users.noreply.github.com>
2023-11-29 14:09:29 +0200

    added dnd provider (#1)

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+  semi: false,
+  endOfLine: "auto",
+  trailingComma: "all",
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+};
+
+module.exports = config;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+
+export default async function Dashboard() {
+  return (
+    <h1>Dash</h1>
+  )
+}
